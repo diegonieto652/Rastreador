97c01e2b9f944f14edea7658082f74315f22d06f
diegonieto652 <37588382+diegonieto652@users.noreply.github.com>
2018-03-20 16:34:37 -0300

    Update rastreador.py

--- rastreador.py
+++ rastreador.py
@@ -6,18 +6,18 @@ import requests
 
 def extract_title(content): #conteúdo a ser examinado
     soup = BeautifulSoup(content, "xml")
-    tag = soup.find("title", text=True) #Encontre a tag title desde que essa tag contenha algum texto
+    tag = soup.find("title", text=True) 
 
-    if not tag: #Não encontrando retorne None, caso encontre retorne o texto dela
+    if not tag: 
         return None
     return tag.string.strip()
 
-def extract_links(content): #função que retorna todos os links presentes no conteudo de uma página
+def extract_links(content): # retorna todos os links presentes no conteudo de uma página
     soup = BeautifulSoup(content, "xml")
     links = set()#Não permitir elementos duplicados
 
-    for tag in soup.find_all("a", href=True):#Retornar todas as tags 'a' que tenham o href preenchido
-        if tag["href"].startswith("http"):#Se essa tag começar com http, eu a entegro aos meus links
+    for tag in soup.find_all("a", href=True):
+        if tag["href"].startswith("http"):
             links.add(tag["href"])
 
     return links
@@ -28,10 +28,10 @@ def crawl(start_url):#recebe como parâmetro a url inicial, por onde começa a n
     available_urls = set([start_url])#urls que existem mas não foram visitadas ainda
 
     while available_urls:
-        url = available_urls.pop()#pego essa url do meu conjunto
+        url = available_urls.pop()
 
         try:
-            content = requests.get(url, timeout=3).text#Tento baixar o conteúdo dessa url com um tempo de 3 segundos
+            content = requests.get(url, timeout=3).text
         except Exception:
             continue
 
